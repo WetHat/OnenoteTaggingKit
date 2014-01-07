9b61a410134b95816873453d70cbadd8c40d7717
WetHat_cp <SND\WetHat_cp>
2014-01-07 17:41:59 +0000

    Migrated to Codeplex from local development environment

---
+++ trunk/OneNoteTaggingKit/edit/TagEditorModel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Interop.OneNote;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WetHatLab.OneNote.TaggingKit.edit
+{
+    /// <summary>
+    /// View Model to support the tag editor dialog
+    /// </summary>
+    public class TagEditorModel : ITagEditorModel
+    {
+        private Application _OneNote;
+
+        private ObservableCollection<string> _pageTags;
+        private ObservableCollection<string> _knownTags;
+
+        private OneNotePageProxy _page;
+
+        internal TagEditorModel(Application onenote, string pageID)
+        {
+            _OneNote = onenote;
+
+            _page = new OneNotePageProxy(_OneNote, _OneNote.Windows.CurrentWindow.CurrentPageId);
+
+            _pageTags = new ObservableCollection<string>(_page.PageTags);
+
+            _knownTags = new ObservableCollection<string>(OneNotePageProxy.ParseTags(Properties.Settings.Default.KnownTags));  
+        }
+
+        #region ITagEditorModel
+        /// <summary>
+        /// Get the collection of page tags.
+        /// </summary>
+        public ObservableCollection<string> PageTags
+        {
+            get { return _pageTags; }
+        }
+
+        /// <summary>
+        /// Get the collection of all tags known to the addin.
+        /// </summary>
+        /// <remarks>These tags are used to suggest page tags</remarks>
+        public ObservableCollection<string> KnownTags
+        {
+            get { return _knownTags; }
+        }
+
+        /// <summary>
+        /// Get the version of the addin.
+        /// </summary>
+        public string AddinVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+        #endregion ITagEditorModel
+
+        internal void SaveChanges()
+        {
+            string[] tags = _pageTags.ToArray();
+            Array.Sort(tags);
+            _page.PageTags = tags;
+            _page.Update();
+
+            Properties.Settings.Default.KnownTags = string.Join(",", _knownTags.Union(_pageTags));
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ trunk/OneNoteTaggingKit/find/TagSelector.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    /// Interaction logic for TagSelector.xaml
+    /// </summary>
+    public partial class TagSelector : UserControl
+    {
+        private TagPageSet _tag;
+
+        /// <summary>
+        /// Event fired when tag is selected.
+        /// </summary>
+        public event RoutedEventHandler Checked;
+        /// <summary>
+        /// Event firen when tag is deselected
+        /// </summary>
+        public event RoutedEventHandler UnChecked;
+
+        /// <summary>
+        /// dependency property for the number of pages having a tag
+        /// </summary>
+        public static readonly DependencyProperty PageCountProperty = DependencyProperty.Register("PageCount", typeof(int), typeof(TagSelector));
+
+        /// <summary>
+        /// Number of pages having this tag
+        /// </summary>
+        public int PageCount
+        {
+            get
+            {
+                return (int)GetValue(PageCountProperty);
+            }
+            set
+            {
+                SetValue(PageCountProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Dependency property to indicate if tag is selected
+        /// </summary>
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register("IsChecked", typeof(bool), typeof(TagSelector), new PropertyMetadata(OnCheckedChanged));
+
+        /// <summary>
+        /// get or set a tags selectes state
+        /// </summary>
+        public bool IsChecked
+        {
+            get
+            {
+                return (bool)GetValue(IsCheckedProperty);
+            }
+            set
+            {
+                if (IsChecked != value)
+                {
+                    SetValue(IsCheckedProperty, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// dependency property for tag's name
+        /// </summary>
+        public static readonly DependencyProperty TagNameProperty = DependencyProperty.Register("TagName", typeof(string), typeof(TagSelector));
+        /// <summary>
+        /// Get or set the tag's name
+        /// </summary>
+        public string TagName
+        {
+            get
+            {
+                return (string)GetValue(TagNameProperty);
+            }
+            set
+            {
+                SetValue(TagNameProperty, value);
+            }
+        }
+
+        internal void UpdateTag()
+        {
+            if (PageCount != _tag.Pages.Count)
+            {
+                PageCount = _tag.Pages.Count;
+            }
+        }
+
+        /// <summary>
+        /// Create a new instance of the button control
+        /// </summary>
+        public TagSelector()
+        {
+            InitializeComponent();
+            TagName = "tag";
+        }
+
+
+        /// <summary>
+        /// get or set the tag.
+        /// </summary>
+        public TagPageSet PageTag
+        {
+            get
+            {
+                return _tag;
+            }
+            set
+            {
+                _tag = value;
+                TagName = _tag.TagName;
+                UpdateTag();
+            }
+        }
+
+        private static void OnCheckedChanged(object sender,DependencyPropertyChangedEventArgs args)
+        {
+            TagSelector ts = sender as TagSelector;
+            if ((bool)args.NewValue)
+            {
+                if (ts.Checked != null)
+                {
+                    ts.Checked(ts, new RoutedEventArgs());
+                }
+            }
+            else
+            {
+                if (ts.UnChecked != null)
+                {
+                    ts.UnChecked(ts, new RoutedEventArgs());
+                }
+            }
+        }
+    }
+}
