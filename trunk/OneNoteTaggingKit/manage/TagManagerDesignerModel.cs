c9fa0e54d9379f7a9ff72df14232aadb38c9c36f
Peter Ernst <WetHat@users.noreply.github.com>
2014-01-07 17:41:59 +0000

    Migrated to Codeplex from local development environment

---
+++ trunk/OneNoteTaggingKit/manage/TagManagerDesignerModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    /// View model to support design mode for the <see cref="TagManager"/> dialog
+    /// </summary>
+    public class TagManagerDesignerModel : ITagManagerModel
+    {
+        private ObservableCollection<string> _tags = new ObservableCollection<string>();
+
+        /// <summary>
+        /// Create a new instance of the view model including some dummy data.
+        /// </summary>
+        public TagManagerDesignerModel()
+        {
+            _tags.Add("Tag 1");
+            _tags.Add("Tag 2");
+        }
+
+        /// <summary>
+        /// Get the collection of tags used for suggestions.
+        /// </summary>
+        public ObservableCollection<string> SuggestedTags
+        {
+            get
+            {
+                return _tags;
+            }
+        }
+
+        /// <summary>
+        /// Get the version string for the addin
+        /// </summary>
+        public string AddinVersion
+        {
+            get { return "1.2.3.4"; }
+        }
+    }
+}
---
+++ trunk/OneNoteTaggingKit/manage/TagManagerModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using WetHatLab.OneNote.TaggingKit.edit;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    /// View model for the <see cref="TagManager"/> dialog.
+    /// </summary>
+    public class TagManagerModel : ITagManagerModel
+    {
+        private ObservableCollection<string> _suggestedTags;
+
+
+        internal TagManagerModel()
+        {
+            _suggestedTags = new ObservableCollection<string>(OneNotePageProxy.ParseTags(Properties.Settings.Default.KnownTags));
+        }
+
+        #region ITagManagerModel
+
+        /// <summary>
+        /// Get the collection of tags used for suggestions.
+        /// </summary>
+        public ObservableCollection<string> SuggestedTags
+        {
+            get
+            {
+                return _suggestedTags;
+            }
+        }
+
+        /// <summary>
+        /// Get the version of the addin.
+        /// </summary>
+        public string AddinVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+        #endregion
+
+        internal void SaveChanges()
+        {
+            string[] t = _suggestedTags.ToArray();
+            Array.Sort(t);
+            Properties.Settings.Default.KnownTags = string.Join(",", t);
+            Properties.Settings.Default.Save();
+        }
+    }
+}
