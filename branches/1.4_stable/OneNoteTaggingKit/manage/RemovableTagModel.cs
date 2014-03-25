5227c6d1e5edcc6fc58f89c41da4316b9b8183ec
Peter Ernst <WetHat@users.noreply.github.com>
2014-03-25 10:08:51 +0000

    Codeplex stable 1.4 release

---
+++ branches/1.4_stable/OneNoteTaggingKit/manage/RemovableTagModel.cs
@@ -0,0 +1,70 @@
+﻿using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    /// View model backing a <see cref="RemovableTag"/> user control.
+    /// </summary>
+    /// <remarks>Provides properties to enable/disable a tag for removal and to adjust the presentation of the corresponding UI element.</remarks>
+    public class RemovableTagModel : ISortableKeyedItem<TagModelKey,string>
+    {
+        private TagPageSet _tag;
+
+        private TagModelKey _sortKey;
+
+        internal RemovableTagModel(TagPageSet tag)
+        {
+            _tag = tag;
+
+            _sortKey = new TagModelKey(tag.TagName);
+        }
+
+        /// <summary>
+        /// Get the tag name
+        /// </summary>
+        public string TagName
+        {
+            get { return _tag.Key; }
+        }
+
+        /// <summary>
+        /// Check whether the tag can be removed
+        /// </summary>
+        public bool CanRemove
+        {
+            get { return UseCount == 0; }
+        }
+
+        /// <summary>
+        /// Get the number of uses of this tag 
+        /// </summary>
+        public int UseCount
+        {
+            get { return _tag.Pages.Count; }
+        }
+
+        /// <summary>
+        /// Get the visibility of the <i>remove</i> marker
+        /// </summary>
+        public System.Windows.Visibility RemoveMarkerVisibility
+        {
+            get { return CanRemove ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed; }
+        }
+
+        #region ISortableKeyedItem<TagModelKey,string>
+        /// <summary>
+        /// Get the name (key) of this tag
+        /// </summary>
+        public string Key
+        {
+            get { return _tag.Key; }
+        }
+        public TagModelKey SortKey
+        {
+            get { return _sortKey; }
+        }
+
+        #endregion ISortableKeyedItem<TagModelKey,string>
+    }
+}
