096287b5362b94320ebec97ec2a7e3ff9861d838
WetHat_cp <SND\WetHat_cp>
2014-04-15 18:42:44 +0000

    TaggingKit 2.0 stable release to the public update 1

---
+++ branches/2.0_stable.1/OneNoteTaggingKit/edit/HitHighlightedTagButtonDesignerModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WetHatLab.OneNote.TaggingKit.edit
+{
+    public class HitHighlightedTagButtonDesignerModel : IHitHighlightedTagButtonModel
+    {
+        public System.Windows.Visibility Visibility
+        {
+            get { return System.Windows.Visibility.Visible; }
+        }
+
+        public string TagName
+        {
+            get { return "Sample Tag"; }
+        }
+
+
+        public Hit Hit
+        {
+            get { return default(Hit); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public Thickness Margin
+        {
+            get { return new Thickness(0,5,5,0); }
+        }
+    }
+}
