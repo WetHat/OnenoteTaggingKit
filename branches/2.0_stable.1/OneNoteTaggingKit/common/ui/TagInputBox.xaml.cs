7c88830db5783f7193eeb492175a99b81f1f1a13
Peter Ernst <WetHat@users.noreply.github.com>
2014-04-15 18:42:44 +0000

    TaggingKit 2.0 stable release to the public update 1

---
+++ branches/2.0_stable.1/OneNoteTaggingKit/common/ui/TagInputBox.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using WetHatLab.OneNote.TaggingKit.edit;
+
+namespace WetHatLab.OneNote.TaggingKit.common.ui
+{
+    public class TagInputEventArgs : RoutedEventArgs
+    {
+        public bool TagInputComplete { get; private set; }
+        internal TagInputEventArgs(RoutedEvent routedEvent, object source, bool enterPressed)
+            : base(routedEvent, source)
+        {
+            TagInputComplete = enterPressed;
+        }
+    }
+    public delegate void TagInputEventHandler(object sender, TagInputEventArgs e);
+
+    /// <summary>
+    /// Interaction logic for TagInputBox.xaml
+    /// </summary>
+    public partial class TagInputBox : UserControl
+    {
+        public static readonly RoutedEvent TagInputEvent = EventManager.RegisterRoutedEvent("TagInput", RoutingStrategy.Bubble, typeof(TagInputEventHandler), typeof(TagInputBox));
+
+        // Provide CLR accessors for the event 
+        public event TagInputEventHandler TagInput
+        {
+            add { AddHandler(TagInputEvent, value); }
+            remove { RemoveHandler(TagInputEvent, value); }
+        }
+
+        public TagInputBox()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return string.IsNullOrEmpty(tagInput.Text);
+            }
+        }
+
+        public IEnumerable<string> Tags
+        {
+            get
+            {
+                return from t in OneNotePageProxy.ParseTags(tagInput.Text) select CultureInfo.CurrentCulture.TextInfo.ToTitleCase(t);
+            }
+            set
+            {
+                tagInput.Text = string.Join(",", value);
+
+                UpdateVisibility();
+            }
+        }
+
+        public bool Focus()
+        {
+            return tagInput.Focus();
+        }
+
+        public void Clear()
+        {
+            tagInput.Text = String.Empty;
+            UpdateVisibility();
+        }
+
+        private void UpdateVisibility()
+        {
+            if (string.IsNullOrEmpty(tagInput.Text))
+            {
+                Background = Brushes.White;
+                tagInput.Background = Brushes.Transparent;
+                clearTagInput.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                Background = Brushes.Transparent;
+                tagInput.Background = Brushes.White;
+                clearTagInput.Visibility = System.Windows.Visibility.Visible;
+            }
+            tagInput.Focus();
+        }
+        private void TagInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            UpdateVisibility();
+            RaiseEvent(new TagInputEventArgs(TagInputEvent, this, e.Key == System.Windows.Input.Key.Enter));
+        }
+
+        private void ClearInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            RaiseEvent(new TagInputEventArgs(TagInputEvent, this, false));
+        }
+    }
+}
---
+++ branches/2.0_stable.1/OneNoteTaggingKit/edit/TagEditorDesignerModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+
+namespace WetHatLab.OneNote.TaggingKit.edit
+{
+    /// <summary>
+    /// View model implementation for the UI designer
+    /// </summary>
+    public class TagEditorDesignerModel : ITagEditorModel
+    {
+        ObservableSortedList<TagModelKey, string, SimpleTagButtonModel> _pageTags = new ObservableSortedList<TagModelKey, string, SimpleTagButtonModel>();
+        ObservableSortedList<TagModelKey, string, HitHighlightedTagButtonModel> _suggestedTags = new ObservableSortedList<TagModelKey, string, HitHighlightedTagButtonModel>();
+
+        /// <summary>
+        /// Create a new instance of the view model
+        /// </summary>
+        public TagEditorDesignerModel()
+        {
+            _suggestedTags.AddAll(new HitHighlightedTagButtonModel[] {
+                new HitHighlightedTagButtonModel("Suggested Tag 1"),
+                new HitHighlightedTagButtonModel("Suggested Tag 2")
+            });
+
+            _pageTags.AddAll(new SimpleTagButtonModel[] { new SimpleTagButtonModel("tag 1"), new SimpleTagButtonModel("tag 2") });
+        }
+
+        /// <summary>
+        /// get the collection of page tags.
+        /// </summary>
+        public ObservableSortedList<TagModelKey, string, SimpleTagButtonModel> PageTags
+        {
+            get { return _pageTags; }
+        }
+
+        public ObservableSortedList<TagModelKey, string, HitHighlightedTagButtonModel> SuggestedTags
+        {
+            get { return _suggestedTags; }
+        }
+
+
+        public IEnumerable<TaggingScopeDescriptor> TaggingScopes
+        {
+            get { return new TaggingScopeDescriptor[] { new TaggingScopeDescriptor(TaggingScope.CurrentNote,"Current Note")}; }
+        }
+    }
+}
