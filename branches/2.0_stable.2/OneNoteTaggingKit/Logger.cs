484a8c4db007f53022851cdddfe8f7903bb9645d
Peter Ernst <WetHat@users.noreply.github.com>
2014-04-17 16:33:21 +0000

    TaggingKit 2.0 stable release to the public update 2

---
+++ branches/2.0_stable.2/OneNoteTaggingKit/Logger.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace WetHatLab.OneNote.TaggingKit
+{
+    internal struct TraceCategory
+    {
+        private static readonly string TRACE_INFO = "INFO";
+        private static readonly string TRACE_WARNING = "WARNING";
+        private static readonly string TRACE_ERROR = "ERROR";
+
+        private string _category;
+        private string _caller;
+        private int    _line;
+        internal string Category   { get { return _category; } }
+        internal string CallerName { get { return _caller;  } }
+        internal int Line          { get { return _line;  } }
+
+        private TraceCategory(string category, string caller, int line)
+        {
+            _category = category;
+            _caller = caller;
+            _line = line;
+        }
+
+        internal static TraceCategory Info([CallerMemberName] string callerName = "", [CallerLineNumber] int line = -1)
+        {
+            return new TraceCategory(TRACE_INFO, callerName, line);
+        }
+
+        internal static TraceCategory Warning([CallerMemberName] string callerName = "", [CallerLineNumber] int line = -1)
+        {
+            return new TraceCategory(TRACE_WARNING, callerName, line);
+        }
+
+        internal static TraceCategory Error([CallerMemberName] string callerName = "", [CallerLineNumber] int line = -1)
+        {
+            return new TraceCategory(TRACE_ERROR, callerName, line);
+        }
+    }
+
+    internal class TraceLogger
+    {
+        static string _logfile = null;
+
+        internal static string LogFile
+        {
+            get
+            {
+                if (_logfile == null)
+                {
+                    _logfile = Path.Combine(Path.GetTempPath(), "taggingkit_" + DateTime.Now.Ticks.ToString("X") + ".log");
+                }
+                return _logfile;
+            }
+        }
+        internal static void Register()
+        {
+            FileStream log = new FileStream(LogFile, FileMode.OpenOrCreate);
+            // Creates the new trace listener.
+            TextWriterTraceListener listener = new TextWriterTraceListener(log);
+            Trace.Listeners.Add(listener);
+
+            Log(TraceCategory.Info(),
+                "{0} logging activated.\r\n\tAddin-Version: {1}\r\n\t.net Framework Version: {2}",
+                Properties.Resources.TaggingKit_About_Appname,
+                Assembly.GetExecutingAssembly().GetName().Version,
+                Environment.Version
+                );
+            Flush();
+        }
+
+        internal static void Log(TraceCategory category, string message, params object[] args)
+        {
+#if TRACE
+            Trace.Write(category.CallerName,category.Category);
+            Trace.Write(" (");
+            Trace.Write(category.Line);
+            Trace.Write(") | ");
+            try
+            {
+                Trace.WriteLine(string.Format(message, args));
+            }
+            catch (Exception ex)
+            {
+                Log(TraceCategory.Error(), "Logging failed {0}", ex);
+            }
+            
+#endif //TRACE
+        }
+
+        internal static void Flush()
+        {
+            Trace.Flush();
+        }
+    }
+}
---
+++ branches/2.0_stable.2/OneNoteTaggingKit/common/TagPageSet.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace WetHatLab.OneNote.TaggingKit.common
+{
+    /// <summary>
+    /// The set of pages which have a specified tag in the &lt;one:Meta name="TaggingKit.PageTags" ...&gt; element
+    /// </summary>
+    public class TagPageSet : IKeyedItem<string>, INotifyPropertyChanged
+    {
+        private static readonly PropertyChangedEventArgs PAGE_COUNT = new PropertyChangedEventArgs("PageCount");
+
+        private HashSet<TaggedPage> _pages = new HashSet<TaggedPage>();
+
+        private HashSet<TaggedPage> _filteredPages;
+
+        /// <summary>
+        /// get name of the tag.
+        /// </summary>
+        public string TagName { get; private set; }
+
+        private void firePropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, args);
+            }
+        }
+
+        internal TagPageSet(string tagName) 
+        {
+            TagName = tagName;
+        }
+
+        internal ISet<TaggedPage> Pages
+        {
+            get
+            {
+                return _filteredPages != null ?_filteredPages : _pages;
+            }
+        }
+
+        internal int PageCount
+        {
+            get
+            {
+                return Pages.Count;
+            }
+        }
+
+        internal bool AddPage(TaggedPage pg)
+        {
+            bool added = _pages.Add(pg);
+
+            if (added)
+            {
+                firePropertyChanged(PAGE_COUNT);
+            }
+            return added;
+        }
+
+        internal bool RemovePage(TaggedPage pg)
+        {
+            bool removed = _pages.Remove(pg);
+
+            if (removed)
+            {
+                firePropertyChanged(PAGE_COUNT);
+            }
+            return removed;
+        }
+
+        internal void ClearFilter()
+        {
+            if (_filteredPages != null)
+            {
+                _filteredPages = null;
+                firePropertyChanged(PAGE_COUNT);
+            }
+        }
+        internal void IntersectWith(IEnumerable<TaggedPage> filter)
+        {
+            int countBefore = PageCount;
+            _filteredPages = new HashSet<TaggedPage>(_pages);
+            _filteredPages.IntersectWith(filter);
+
+            if (countBefore != PageCount)
+            {
+                firePropertyChanged(PAGE_COUNT);
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            TagPageSet other = obj as TagPageSet;
+
+            if (other != null)
+            {
+                return TagName.Equals(other.TagName);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return TagName.GetHashCode();
+        }
+
+        #region IKeyedItem
+
+        /// <summary>
+        /// Get the tag's sortable key
+        /// </summary>
+        public string Key
+        {
+            get { return TagName; }
+        }
+        #endregion IKeyedItem
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
