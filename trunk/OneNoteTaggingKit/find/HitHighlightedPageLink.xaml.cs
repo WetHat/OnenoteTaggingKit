12c98251098596b511207f50b6a824fa7152c6e2
WetHat_cp <SND\WetHat_cp>
2014-04-17 07:08:01 +0000

    Tooltip on found pages improved

--- trunk/OneNoteTaggingKit/find/HitHighlightedPageLink.xaml.cs
+++ trunk/OneNoteTaggingKit/find/HitHighlightedPageLink.xaml.cs
@@ -58,19 +58,46 @@ namespace WetHatLab.OneNote.TaggingKit.find
 
             // rebuild the hit highlighted Tooltip
             ToolTip tt = new ToolTip();
-            tt.Style = new Style();
-            TextBlock tb = new TextBlock();
-            tb.MaxWidth = 300;
-            tb.TextWrapping = TextWrapping.Wrap;
-            tt.Content = tb;
+            tt.Style = new Style(); // override the global style
 
+            StackPanel stack = new StackPanel();
+            stack.Orientation = Orientation.Vertical;
+            tt.Content = stack;
+
+            TextBlock path = new TextBlock();
+            path.Foreground = Brushes.Gray;
+            //path.FontSize = 10;
+            path.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            path.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            foreach (var he in model.Path)
+            {
+                if (path.Inlines.Count > 0)
+                {
+                    Run r = new Run(" ñ ");
+                    r.FontFamily = new FontFamily("Symbol");
+                    r.FontWeight = FontWeights.ExtraBold;
+                    r.Foreground = Brushes.Black;
+                    path.Inlines.Add(r);
+                }
+                path.Inlines.Add(new Run(he.Name));
+            }
+            stack.Children.Add(path);
+            
+            TextBlock tb = new TextBlock();
+            tb.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            
             foreach (Run r in link.hithighlightedTitle.Inlines)
             {
                 Run newR = new Run(r.Text);
                 newR.Background = r.Background;
+                newR.FontSize = r.FontSize + 2;
+                newR.FontWeight = FontWeights.Medium;
                 tb.Inlines.Add(newR);
             }
+            stack.Children.Add(tb);
+
             link.ToolTip = tt;
+            ToolTipService.SetShowDuration(link, 10000);
         }
 
         public HitHighlightedPageLink()
