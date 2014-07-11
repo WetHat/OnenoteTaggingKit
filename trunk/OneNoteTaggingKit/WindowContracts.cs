10cb429327892b55032419282adbd8a424870612
Peter Ernst <WetHat@users.noreply.github.com>
2014-07-11 13:23:40 +0000

    WindowViewModelBase relocated to ui namespace. Some minor cleanuo

--- trunk/OneNoteTaggingKit/WindowContracts.cs
+++ trunk/OneNoteTaggingKit/WindowContracts.cs
@@ -1,10 +1,10 @@
-﻿using WetHatLab.OneNote.TaggingKit.common;
+﻿using WetHatLab.OneNote.TaggingKit.common.ui;
 namespace WetHatLab.OneNote.TaggingKit
 {
     /// <summary>
     /// interface for dialogs opened by this addin 
     /// </summary>
-    /// <typeparam name="M"></typeparam>
+    /// <typeparam name="M">type of the view model backing the window</typeparam>
     internal interface IOneNotePageWindow<M> where M: WindowViewModelBase
     {
         /// <summary>
