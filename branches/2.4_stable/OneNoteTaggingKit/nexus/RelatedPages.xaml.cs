84b7b6b83327219bb3b2f763983184299ab1ae43
Peter Ernst <WetHat@users.noreply.github.com>
2014-07-22 18:17:20 +0000

    Tagging Kit 2.4 stable release

---
+++ branches/2.4_stable/OneNoteTaggingKit/nexus/RelatedPages.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WetHatLab.OneNote.TaggingKit.nexus
+{
+    /// <summary>
+    /// Interaction logic for RelatedPages.xaml
+    /// </summary>
+    public partial class RelatedPages : Window, IOneNotePageWindow<RelatedPagesModel>
+    {
+        private RelatedPagesModel _model;
+        public RelatedPages()
+        {
+            InitializeComponent();
+        }
+
+        #region IOneNotePageWindow<RelatedPagesModel>
+        public RelatedPagesModel ViewModel
+        {
+            get
+            {
+                return _model;  
+            }
+            set
+            {
+                _model = value;
+                DataContext = _model;
+                _model.TrackCurrentPage();
+            }
+        }
+        #endregion IOneNotePageWindow<RelatedPagesModel>
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_model != null)
+            {
+                _model.Dispose();
+                _model = null;
+            }
+        }
+
+    }
+}
