e6b3132a0e09fecd21e91d223743e88d546c8a04
Peter Ernst <WetHat@users.noreply.github.com>
2015-07-23 06:41:24 +0000

    Stable 2.7 release

---
+++ branches/2.7_stable/OneNoteTaggingKit/manage/RemovableTag.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WetHatLab.OneNote.TaggingKit.find;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    /// Interaction logic for RemovableTag.xaml
+    /// </summary>
+    [ComVisible(false)]
+    public partial class RemovableTag : UserControl
+    {
+        /// <summary>
+        /// Click event for this button.
+        /// </summary>
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(RemovableTag));
+
+        /// <summary>
+        /// create a new instance of a <see cref="RemovableTag"/> user control
+        /// </summary>
+        public RemovableTag()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Add or remove the click handler.
+        /// </summary>
+        /// <remarks>clr wrapper for routed event</remarks>
+        internal event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RoutedEventArgs newClickEventArgs = new RoutedEventArgs(ClickEvent, this);
+
+            RaiseEvent(newClickEventArgs);
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            RemovableTag t = sender as RemovableTag;
+
+            RemovableTagModel mdl = t.DataContext as RemovableTagModel;
+            if (mdl != null)
+            {
+                mdl.PropertyChanged += mdl_PropertyChanged;
+                mdl_PropertyChanged(mdl,RemovableTagModel.HIGHLIGHTED_TAGNAME);
+            }
+        }
+
+        void mdl_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            RemovableTagModel mdl = sender as RemovableTagModel;
+            if (mdl != null)
+            {
+                if (e == RemovableTagModel.HIGHLIGHTED_TAGNAME)
+                {
+                    highlighedTag.Inlines.Clear();
+                    highlighedTag.Inlines.AddRange(mdl.HighlightedTagName.Select((f) =>
+                    {
+                        Run r = new Run(f.Text);
+                        if (f.IsMatch)
+                        {
+                            r.Background = Brushes.Yellow;
+                        }
+                        return r;
+                    }));
+                }
+            }
+        }
+    }
+}
---
+++ branches/2.7_stable/OneNoteTaggingKit/manage/TagManagerModel.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Office.Interop.OneNote;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+using WetHatLab.OneNote.TaggingKit.edit;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    ///  Contract for view models of the <see cref="WetHatLab.OneNote.TaggingKit.manage.TagManager"/> dialog
+    /// </summary>
+    /// <seealso cref="TagManager"/>
+
+    internal interface ITagManagerModel
+    {
+        /// <summary>
+        /// Get the collection of all tags used for suggestions.
+        /// </summary>
+        SuggestedTagsSource<RemovableTagModel> SuggestedTags { get; }
+
+        /// <summary>
+        /// Get the add-in version.
+        /// </summary>
+        string AddinVersion { get; }
+
+        /// <summary>
+        /// Get the add-in version.
+        /// </summary>
+        string FrameworkVersion { get; }
+
+        /// <summary>
+        /// Get the location of the logfile
+        /// </summary>
+        string Logfile { get; }
+    }
+
+    /// <summary>
+    /// View model backing the <see cref="TagManager"/> dialog.
+    /// </summary>
+    [ComVisible(false)]
+    public class TagManagerModel : WindowViewModelBase, ITagManagerModel
+    {
+        private SuggestedTagsSource<RemovableTagModel> _suggestedTags = new SuggestedTagsSource<RemovableTagModel>();
+        private TagsAndPages _tags;
+
+        /// <summary>
+        /// Create a new instance of the view model backing the <see cref="TagManager"/> dialog.
+        /// </summary>
+        /// <param name="onenote">OneNote application object</param>
+        /// <param name="schema">current schema version</param>
+        internal TagManagerModel(Application onenote, XMLSchema schema): base(onenote,schema)
+        {
+            _tags = new TagsAndPages(OneNoteApp, OneNotePageSchema);
+        }
+
+        internal async Task LoadSuggestedTagsAsync()
+        {
+            Task onenotetags = Task.Run(() => _tags.FindTaggedPages(String.Empty));
+            Task suggestions = _suggestedTags.LoadSuggestedTagsAsync();
+            await Task.WhenAll(onenotetags, suggestions);
+            // update the tags loaded from the settings
+            foreach (var t in _suggestedTags.Values)
+            {
+                TagPageSet tag;
+                if (_tags.Tags.TryGetValue(t.TagName, out tag))
+                {
+                    t.Tag = tag;
+                }
+            }
+        }
+       
+        #region ITagManagerModel
+
+        /// <summary>
+        /// Get the collection of tags used for suggestions.
+        /// </summary>
+        /// <remarks>This collection includes all tags used on any OneNote pages and additional tags suggestions
+        /// which were added manually</remarks>
+        public SuggestedTagsSource<RemovableTagModel> SuggestedTags
+        {
+            get
+            {
+                return _suggestedTags;
+            }
+        }
+
+        /// <summary>
+        /// Get the version of the addin.
+        /// </summary>
+        public string AddinVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+        public string FrameworkVersion
+        {
+            get { return Environment.Version.ToString(); }
+        }
+
+        public string Logfile
+        {
+            get { return TraceLogger.LogFile; }
+        }
+        #endregion
+
+        /// <summary>
+        /// Get comma separated list of suggested tags.
+        /// </summary>
+        public string TagList
+        {
+            get
+            {
+                StringBuilder tags = new StringBuilder();
+                foreach (var t in _suggestedTags.Values)
+                {
+                    if (tags.Length > 0)
+                    {
+                        tags.Append(',');
+                    }
+                    tags.Append(t.TagName);
+                }
+                return tags.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Persist any changes
+        /// </summary>
+        internal void SaveChanges()
+        {
+            _suggestedTags.Save();
+        }
+    }
+}
