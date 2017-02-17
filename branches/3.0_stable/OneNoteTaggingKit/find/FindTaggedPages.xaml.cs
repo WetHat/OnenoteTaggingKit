f4d4dfe0cc97929a0eef0c81734537b68b0d81bd
Peter Ernst <WetHat@users.noreply.github.com>
2017-02-17 07:55:25 +0000

    3.0 Stable Release

---
+++ branches/3.0_stable/OneNoteTaggingKit/find/FindTaggedPages.xaml.cs
@@ -0,0 +1,352 @@
+﻿// Author: WetHat | (C) Copyright 2013 - 2017 WetHat Lab, all rights reserved
+using System.Linq;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+using System;
+using System.Text;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    /// Interaction logic for TagSearch.xaml
+    /// </summary>
+    [ComVisible(false)]
+    public partial class FindTaggedPages : Window, IOneNotePageWindow<FindTaggedPagesModel>
+    {
+        private FindTaggedPagesModel _model;
+
+        /// <summary>
+        /// Create a new instance of the find tags window
+        /// </summary>
+        public FindTaggedPages()
+        {
+            InitializeComponent();
+        }
+
+        #region IOneNotePageWindow<FindTaggedPagesModel>
+
+        /// <summary>
+        /// get or set the view model backing this UI
+        /// </summary>
+        public FindTaggedPagesModel ViewModel
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+                _model.PropertyChanged += _model_PropertyChanged;
+                DataContext = _model;
+            }
+        }
+
+        #endregion IOneNotePageWindow<FindTaggedPagesModel>
+
+        #region UI events
+
+        private async void Page_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            if (item != null)
+            {
+                switch (item.Tag.ToString())
+                {
+                    case "Refresh":
+                        string query = searchComboBox.Text;
+
+                        try
+                        {
+                            pBar.Visibility = System.Windows.Visibility.Visible;
+                            await _model.FindPagesAsync(query, scopeSelect.SelectedScope);
+                            searchComboBox.SelectedValue = query;
+                            pBar.Visibility = System.Windows.Visibility.Hidden;
+                        }
+                        catch (System.Exception ex)
+                        {
+                            TraceLogger.Log(TraceCategory.Error(), "search for '{0}' failed: {1}", query, ex);
+                            TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_Find, ex);
+                        }
+                        break;
+
+                    case "ClearSelection":
+                        foundPagesList.UnselectAll();
+                        break;
+
+                    case "SelectAll":
+                        foundPagesList.SelectAll();
+                        break;
+
+                    case "CopyLinks":
+                        string header =
+@"Version:0.9
+StartHTML:{0:D6}
+EndHTML:{1:D6}
+StartFragment:{2:D6}
+EndFragment:{3:D6}
+StartSelection:{4:D6}
+EndSelection:{5:D6}";
+                        string htmlpre =
+@"<HTML>
+<BODY>
+<!--StartFragment-->";
+                        StringBuilder links = new StringBuilder();
+
+                        foreach (var mdl in _model.Pages.Where((p) => p.IsSelected))
+                        {
+                            string pageTitle = mdl.LinkTitle;
+                            try
+                            {
+                                if (links.Length > 0)
+                                {
+                                    links.Append("<br />");
+                                }
+                                links.Append(@"<a href=""");
+                                links.Append(mdl.PageLink);
+                                links.Append(@""">");
+                                links.Append(mdl.LinkTitle);
+                                links.Append("</a>");
+                            }
+                            catch (Exception ex)
+                            {
+                                TraceLogger.Log(TraceCategory.Error(), "Link to page '{0}' could not be created: {1}", pageTitle, ex);
+                                TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_CopyLink, ex);
+                            }
+                        }
+
+                        string htmlpost =
+@"<!--EndFragment-->
+</BODY>
+</HTML>";
+                        string strLinks = links.ToString();
+                        string clip = string.Format(header,
+                            header.Length,
+                            header.Length + htmlpre.Length + strLinks.Length + htmlpost.Length,
+                            header.Length + htmlpre.Length,
+                            header.Length + htmlpre.Length + strLinks.Length,
+                            header.Length + htmlpre.Length,
+                            header.Length + htmlpre.Length + strLinks.Length)
+                            + htmlpre + strLinks + htmlpost;
+                        Clipboard.SetText(clip, TextDataFormat.Html);
+                        break;
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // save the scope Properties.Settings.Default.DefaultScope = (int)scopeSelect.SelectedScope;
+            Properties.Settings.Default.Save();
+            if (_model != null)
+            {
+                _model.Dispose();
+            }
+
+            Trace.Flush();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HitHighlightedPageLink l = sender as HitHighlightedPageLink;
+                if (l != null)
+                {
+                    HitHighlightedPageLinkModel model = l.DataContext as HitHighlightedPageLinkModel;
+                    _model.NavigateTo(model.PageID);
+                    if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+                    {
+                        int ndx = foundPagesList.SelectedItems.IndexOf(model);
+                        if (ndx >= 0)
+                        {
+                            foundPagesList.SelectedItems.RemoveAt(ndx);
+                        }
+                        else
+                        {
+                            foundPagesList.SelectedItems.Add(model);
+                        }
+                    }
+                    else
+                    {
+                        // select the link
+                        foundPagesList.SelectedItem = model;
+                    }
+
+                    e.Handled = true;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TraceLogger.Log(TraceCategory.Error(), "Navigation to OneNote page failed: {0}", ex);
+                TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_PageNavigation, ex);
+            }
+        }
+
+        private async void ClearSelectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            pBar.Visibility = System.Windows.Visibility.Visible;
+            await _model.ClearTagFilterAsync();
+            pBar.Visibility = System.Windows.Visibility.Hidden;
+            foreach (var t in _model.Tags.Values)
+            {
+                t.IsChecked = false;
+            }
+            e.Handled = true;
+        }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = searchComboBox.Text;
+
+            try
+            {
+                pBar.Visibility = System.Windows.Visibility.Visible;
+                await _model.FindPagesAsync(query, scopeSelect.SelectedScope);
+                searchComboBox.SelectedValue = query;
+                pBar.Visibility = System.Windows.Visibility.Hidden;
+            }
+            catch (System.Exception ex)
+            {
+                TraceLogger.Log(TraceCategory.Error(), "search for '{0}' failed: {1}", query, ex);
+                TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_Find, ex);
+            }
+            e.Handled = true;
+        }
+
+        private async void SearchComboBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                string query = searchComboBox.Text;
+                pBar.Visibility = System.Windows.Visibility.Visible;
+                await _model.FindPagesAsync(query, scopeSelect.SelectedScope);
+                pBar.Visibility = System.Windows.Visibility.Hidden;
+                searchComboBox.SelectedValue = query;
+            }
+            e.Handled = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            searchComboBox.Focus();
+            Keyboard.Focus(searchComboBox);
+        }
+
+        private void TagInputBox_Input(object sender, TagInputEventArgs e)
+        {
+            if (!e.TagInputComplete)
+            {
+                tagsPanel.Highlighter = new TextSplitter(tagInput.Tags);
+            }
+        }
+
+        private void ScopeSelector_ScopeChanged(object sender, ScopeChangedEventArgs e)
+        {
+            try
+            {
+                pBar.Visibility = System.Windows.Visibility.Visible;
+                string query = searchComboBox.Text;
+                // using ContinueWith until I've discovered how to do implement async
+                // events properly
+                _model.FindPagesAsync(query, scopeSelect.SelectedScope).ContinueWith(tsk => Dispatcher.Invoke(() =>
+                {
+                    pBar.Visibility = System.Windows.Visibility.Hidden;
+                    searchComboBox.SelectedValue = query;
+                }));
+            }
+            catch (System.Exception ex)
+            {
+                TraceLogger.Log(TraceCategory.Error(), "Changing search scope failed: {0}", ex);
+                TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_ScopeChange, ex);
+            }
+            e.Handled = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Stop tracking current page
+            ViewModel.EndTracking();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // start tracking current page
+            ViewModel.BeginTracking();
+        }
+
+        #endregion UI events
+
+        private void _model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e == FindTaggedPagesModel.PAGE_COUNT)
+            {
+                foundPagesList.UnselectAll();
+            }
+            else if (e == FindTaggedPagesModel.CURRENT_TAGS)
+            {
+                // update query if necessary
+                string thisScopID;
+                switch (scopeSelect.SelectedScope)
+                {
+                    case SearchScope.Notebook:
+                        thisScopID = ViewModel.OneNoteApp.CurrentNotebookID;
+                        break;
+
+                    case SearchScope.SectionGroup:
+                        thisScopID = ViewModel.OneNoteApp.CurrentSectionGroupID;
+                        break;
+
+                    case SearchScope.Section:
+                        thisScopID = ViewModel.OneNoteApp.CurrentSectionID;
+                        break;
+
+                    default:
+                        thisScopID = string.Empty;
+                        break;
+                }
+                if (!thisScopID.Equals(ViewModel.LastScopeID))
+                { // rerun the query for the current scope
+                    try
+                    {
+                        pBar.Visibility = System.Windows.Visibility.Visible;
+                        string query = searchComboBox.Text;
+                        _model.FindPagesAsync(query, scopeSelect.SelectedScope).Wait();
+                        tagInput.Tags = ViewModel.CurrentTags;
+                        pBar.Visibility = System.Windows.Visibility.Hidden;
+                        searchComboBox.SelectedValue = query;
+                    }
+                    catch (System.Exception ex)
+                    {
+                        TraceLogger.Log(TraceCategory.Error(), "Changing search scope failed: {0}", ex);
+                        TraceLogger.ShowGenericErrorBox(Properties.Resources.TagSearch_Error_ScopeChange, ex);
+                    }
+                }
+                else
+                {
+                    tagInput.Tags = ViewModel.CurrentTags;
+                }
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (HitHighlightedPageLinkModel pl in e.RemovedItems)
+            {
+                pl.IsSelected = false;
+            }
+            foreach (HitHighlightedPageLinkModel pl in e.AddedItems)
+            {
+                pl.IsSelected = true;
+            }
+        }
+    }
+}
---
+++ branches/3.0_stable/OneNoteTaggingKit/manage/TagManager.xaml.cs
@@ -0,0 +1,229 @@
+﻿// Author: WetHat | (C) Copyright 2013 - 2017 WetHat Lab, all rights reserved
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+using WetHatLab.OneNote.TaggingKit.Tagger;
+
+namespace WetHatLab.OneNote.TaggingKit.manage
+{
+    /// <summary>
+    /// Interaction logic for TagManager.xaml user control
+    /// </summary>
+    /// <remarks>Implements the tag management dialog logic</remarks>
+    [ComVisible(false)]
+    public partial class TagManager : Window, IOneNotePageWindow<TagManagerModel>
+    {
+        private TagManagerModel _model;
+
+        /// <summary>
+        /// Create a new instance of a tag management dialog.
+        /// </summary>
+        public TagManager()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Remove or rename tag from suggestions when user control is tapped.
+        /// </summary>
+        /// <param name="sender">user control emitting this event</param>
+        /// <param name="e">     event details</param>
+        private void Tag_Action(object sender, RoutedEventArgs e)
+        {
+            var rt = sender as RemovableTag;
+            var rt_mdl = rt.DataContext as RemovableTagModel;
+            string[] toRemove = new string[] { rt_mdl.Key };
+            if ("DeleteTag".Equals(rt.Tag))
+            {
+                _model.SuggestedTags.RemoveAll(toRemove);
+                // schedule all pages with this tag for tag removal
+                if (rt_mdl.Tag != null)
+                {
+                    foreach (var tp in rt_mdl.Tag.Pages)
+                    {
+                        _model.OneNoteApp.TaggingService.Add(new TaggingJob(tp.ID, toRemove, TagOperation.SUBTRACT));
+                    }
+                    suggestedTags.Notification = rt_mdl.Tag.Pages.Count == 0 ? Properties.Resources.TagEditor_Popup_NothingTagged : string.Format(Properties.Resources.TagEditor_Popup_TaggingInProgress, rt_mdl.Tag.Pages.Count);
+                    TraceLogger.Log(TraceCategory.Info(), "{0} page(s) enqueued for background tagging; Operation SUBTRACT {1}", rt_mdl.Tag.Pages.Count, toRemove[0]);
+                }
+                else
+                {
+                    suggestedTags.Notification = Properties.Resources.TagEditor_Popup_NothingTagged;
+                }
+            }
+            else if ("RenameTag".Equals(rt.Tag))
+            {
+                _model.SuggestedTags.RemoveAll(toRemove);
+
+                string[] newTagNames = (from tn in OneNotePageProxy.ParseTags(rt_mdl.LocalName) select CultureInfo.CurrentCulture.TextInfo.ToTitleCase(tn)).ToArray();
+
+                // create new tag models unless they already exist
+                List<RemovableTagModel> newTagModels = new List<RemovableTagModel>();
+                foreach (var newName in newTagNames)
+                {
+                    RemovableTagModel tagmodel;
+                    if (!_model.SuggestedTags.TryGetValue(newName, out tagmodel))
+                    {
+                        tagmodel = new RemovableTagModel() { Tag = new TagPageSet(newName) };
+                        newTagModels.Add(tagmodel);
+                    }
+                    else if (tagmodel.Tag == null && rt_mdl.Tag != null)
+                    {
+                        tagmodel.Tag = new TagPageSet(newName);
+                    }
+
+                    if (rt_mdl.Tag != null)
+                    {
+                        // copy the pages into the new tag and update the tag count
+                        foreach (var pg in rt_mdl.Tag.Pages)
+                        {
+                            tagmodel.Tag.Pages.Add(pg);
+                        }
+                        tagmodel.UseCount = tagmodel.Tag.Pages.Count;
+                    }
+                }
+                _model.SuggestedTags.AddAll(newTagModels);
+
+                if (rt_mdl.Tag != null)
+                {
+                    // remove the old tag and add new tag to the pages
+                    foreach (var tp in rt_mdl.Tag.Pages)
+                    {
+                        _model.OneNoteApp.TaggingService.Add(new TaggingJob(tp.ID, toRemove, TagOperation.SUBTRACT));
+                        _model.OneNoteApp.TaggingService.Add(new TaggingJob(tp.ID, newTagNames, TagOperation.UNITE));
+                    }
+                    suggestedTags.Notification = rt_mdl.Tag.Pages.Count == 0 ? Properties.Resources.TagEditor_Popup_NothingTagged : string.Format(Properties.Resources.TagEditor_Popup_TaggingInProgress, rt_mdl.Tag.Pages.Count);
+                    TraceLogger.Log(TraceCategory.Info(), "{0} page(s) enqueued for background tagging; Operation UNITE {1} SUBTRACT {2}", rt_mdl.Tag.Pages.Count, string.Join(",", newTagNames), toRemove[0]);
+                }
+                else
+                {
+                    suggestedTags.Notification = Properties.Resources.TagEditor_Popup_NothingTagged;
+                }
+            }
+            TraceLogger.Flush();
+            _model.SaveChanges();
+        }
+
+        /// <summary>
+        /// Add a new tag to the list of suggestions when the button is pressed
+        /// </summary>
+        /// <param name="sender">control emitting the event</param>
+        /// <param name="e">     event details</param>
+        private void NewTagButton_Click(object sender, RoutedEventArgs e)
+        {
+            _model.SuggestedTags.AddAll(from t in tagInput.Tags where !_model.SuggestedTags.ContainsKey(t) select new RemovableTagModel() { Tag = new TagPageSet(t) });
+            suggestedTags.Highlighter = null;
+            tagInput.Clear();
+            _model.SaveChanges();
+            e.Handled = true;
+        }
+
+        #region IOneNotePageDialog<TagManagerModel>
+
+        /// <summary>
+        /// Get or set the dialog's view model.
+        /// </summary>
+        /// <remarks>
+        /// As soon as the view model is defined a background collection of tags is started
+        /// </remarks>
+        public TagManagerModel ViewModel
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+                DataContext = _model;
+            }
+        }
+
+        #endregion IOneNotePageDialog<TagManagerModel>
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            Trace.Flush();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Hyperlink hl = (Hyperlink)sender;
+
+            string navigateUri = hl.NavigateUri.ToString();
+
+            Process.Start(new ProcessStartInfo(navigateUri));
+
+            e.Handled = true;
+        }
+
+        private async void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var itm = sender as MenuItem;
+            switch (itm.Tag.ToString())
+            {
+                case "Copy":
+                    Clipboard.SetData(DataFormats.Text, _model.TagList);
+                    tagInput.FocusInput();
+                    break;
+
+                case "Refresh":
+                    if (pBar.Visibility == System.Windows.Visibility.Hidden)
+                    {
+                        pBar.Visibility = System.Windows.Visibility.Visible;
+                        await _model.LoadSuggestedTagsAsync();
+                        if (tagInput.Tags != null)
+                        {
+                            suggestedTags.Highlighter = new TextSplitter(tagInput.Tags);
+                        }
+                        pBar.Visibility = System.Windows.Visibility.Hidden;
+                    }
+                    Properties.Settings.Default.Save();
+                    TraceLogger.Flush();
+                    break;
+            }
+        }
+
+        private void TagInputBox_Input(object sender, TagInputEventArgs e)
+        {
+            if (e.TagInputComplete)
+            {
+                NewTagButton_Click(sender, e);
+            }
+            else
+            {
+                suggestedTags.Highlighter = new TextSplitter(tagInput.Tags);
+                e.Handled = true;
+            }
+        }
+
+        private void Hyperlink_RequestLogNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            TraceLogger.Flush();
+            Hyperlink hl = (Hyperlink)sender;
+
+            string path = hl.NavigateUri.LocalPath;
+
+            Process.Start(new ProcessStartInfo("notepad.exe", path));
+
+            e.Handled = true;
+        }
+
+        private async void TabItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (pBar.Visibility == System.Windows.Visibility.Visible)
+            {
+                await _model.LoadSuggestedTagsAsync();
+                pBar.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+    }
+}
