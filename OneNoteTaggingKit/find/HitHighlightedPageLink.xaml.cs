6a916c9f5a65283659de629210cad2675d70ebf2
WetHat <PeterErnst@t-online.de>
2017-07-11 11:37:39 +0200

    Codeplex migration: Conversion from svn to git layout

---
+++ OneNoteTaggingKit/find/HitHighlightedPageLink.xaml.cs
@@ -0,0 +1,106 @@
+﻿// Author: WetHat | (C) Copyright 2013 - 2017 WetHat Lab, all rights reserved
+// Author: WetHat | (C) Copyright 2013 - 2016 WetHat Lab, all rights reserved
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using WetHatLab.OneNote.TaggingKit.common;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    /// Control to render a hit highlighted link to a OneNote page.
+    /// </summary>
+    [ComVisible(false)]
+    public partial class HitHighlightedPageLink : UserControl
+    {
+        /// <summary>
+        /// Routed Event for clicks on links to OneNote pages.
+        /// </summary>
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(HitHighlightedPageLink));
+
+        /// <summary>
+        /// Event fired for clicks on links to OneNote pages.
+        /// </summary>
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            HitHighlightedPageLink link = sender as HitHighlightedPageLink;
+            HitHighlightedPageLinkModel model = e.NewValue as HitHighlightedPageLinkModel;
+            // build the highlighted inline Text
+
+            // rebuild the hithighlighted Title
+            link.hithighlightedTitle.Inlines.Clear();
+
+            foreach (TextFragment f in model.HighlightedTitle)
+            {
+                Run r = new Run(f.Text);
+                if (f.IsMatch)
+                {
+                    r.Background = Brushes.Yellow;
+                }
+                link.hithighlightedTitle.Inlines.Add(r);
+            }
+
+            // rebuild the hit highlighted Tooltip
+            ToolTip tt = new ToolTip();
+            tt.Style = new Style(); // override the global style
+
+            StackPanel stack = new StackPanel();
+            stack.Orientation = Orientation.Vertical;
+            tt.Content = stack;
+
+            TextBlock path = new TextBlock();
+            path.Foreground = Brushes.Gray;
+            //path.FontSize = 10;
+            path.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            path.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            foreach (var he in model.Path)
+            {
+                if (path.Inlines.Count > 0)
+                {
+                    Run r = new Run(" ñ ");
+                    r.FontFamily = new FontFamily("Symbol");
+                    r.FontWeight = FontWeights.ExtraBold;
+                    r.Foreground = Brushes.Black;
+                    path.Inlines.Add(r);
+                }
+                path.Inlines.Add(new Run(he.Name));
+            }
+            stack.Children.Add(path);
+
+            TextBlock tb = new TextBlock();
+            tb.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+
+            foreach (Run r in link.hithighlightedTitle.Inlines)
+            {
+                Run newR = new Run(r.Text);
+                newR.Background = r.Background;
+                newR.FontSize = r.FontSize + 2;
+                newR.FontWeight = FontWeights.Medium;
+                tb.Inlines.Add(newR);
+            }
+            stack.Children.Add(tb);
+
+            ToolTip = tt;
+            ToolTipService.SetShowDuration(link, 10000);
+        }
+
+        public HitHighlightedPageLink()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ClickEvent));
+        }
+    }
+}
---
+++ OneNoteTaggingKit/find/HitHighlightedPageLinkDesignerModel.cs
@@ -0,0 +1,41 @@
+﻿// Author: WetHat | (C) Copyright 2013 - 2017 WetHat Lab, all rights reserved
+// Author: WetHat | (C) Copyright 2013 - 2016 WetHat Lab, all rights reserved
+using System.Collections.Generic;
+using System.Windows.Media;
+using WetHatLab.OneNote.TaggingKit.common;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    /// Design time view model for the <see cref="HitHighlightedPageLink"/> control.
+    /// </summary>
+    public class HitHighlightedPageLinkDesignerModel : IHitHighlightedPageLinkModel
+    {
+        private string _pageTitle = "Test Page Title";
+
+        /// <summary>
+        /// create a new instance of the design time view model.
+        /// </summary>
+        /// <remarks>this constructor is called by the UI design application</remarks>
+        public HitHighlightedPageLinkDesignerModel()
+        {
+        }
+
+        #region IHitHighlightedPageLinkModel
+
+        public IList<TextFragment> HighlightedTitle
+        {
+            get
+            {
+                TextSplitter splitter = new TextSplitter("page");
+                return splitter.SplitText(_pageTitle);
+            }
+        }
+
+        public string MarkerSymbol { get { return "❱"; } }
+
+        public Brush MarkerColor { get { return Brushes.DarkOrange; } }
+
+        #endregion IHitHighlightedPageLinkModel
+    }
+}
