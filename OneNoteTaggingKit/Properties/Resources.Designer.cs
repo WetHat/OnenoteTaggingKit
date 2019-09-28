6cef55fe57cb3dbe9df764d315ca7047704c7b3a
WetHat <PeterErnst@t-online.de>
2019-09-28 08:18:40 +0200

    Copyright Update

--- OneNoteTaggingKit/Properties/Resources.Designer.cs
+++ OneNoteTaggingKit/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace WetHatLab.OneNote.TaggingKit.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -429,7 +429,7 @@ namespace WetHatLab.OneNote.TaggingKit.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Copyright (c) 2013-2018 WetHat Lab.
+        ///   Looks up a localized string similar to Copyright (c) 2013-2019 WetHat Lab.
         /// </summary>
         public static string TaggingKit_About_Copyright {
             get {
