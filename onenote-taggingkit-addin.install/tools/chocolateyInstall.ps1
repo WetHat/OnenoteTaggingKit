8e7c3f0a421543ed2718ee7ea0561c2196a51f10
WetHat <PeterErnst@t-online.de>
2020-11-11 10:36:14 +0100

    Pending reboots ignored

--- onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
+++ onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$packageName   = 'onenote-taggingkit-addin.install'
-$url           = 'https://github.com/WetHat/OnenoteTaggingKit/releases/download/v3.7.7581/SetupTaggingKitWiX.3.7.7581.19466.msi'
+$url           = 'https://github.com/WetHat/OnenoteTaggingKit/releases/download/v3.8.7617/SetupTaggingKitWiX.3.8.7617.36152.msi'
 $silentArgs    = '/qn' 
-$validExitCodes = @(0) 
+$validExitCodes = @(0,1603) 
 
 Install-ChocolateyPackage -packageName   $packageName `
                           -FileType      'MSI'        `
                           -SilentArgs     $silentArgs `
                           -Url            $url `
-                          -Checksum       '51223d2e3fc66456fd79b0d9c10981c4f9abcd27506bb4a87ebaa9f001ccced6' `
+                          -Checksum       '2a1a0ab90227b80d49b92131f048f65e22df5c92b2f46a5f579cdd1c30478fe6' `
                           -ChecksumType   'sha256' `
                           -validExitCodes $validExitCodes
