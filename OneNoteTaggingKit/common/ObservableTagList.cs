c614868ecd947aea49fa9af17a65333f4a4a9b05
WetHat <PeterErnst@t-online.de>
2021-12-26 12:47:21 +0100

    Configure parent class to dispose removed items

--- OneNoteTaggingKit/common/ObservableTagList.cs
+++ OneNoteTaggingKit/common/ObservableTagList.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WetHatLab.OneNote.TaggingKit.common.ui;
 
 namespace WetHatLab.OneNote.TaggingKit.common
@@ -23,7 +21,14 @@ namespace WetHatLab.OneNote.TaggingKit.common
 
     /// <summary>
     /// A generic observable list of sorted page tag models.
-    /// </summary>>
+    /// </summary>
+    /// <remarks>
+    /// Items in this list are disposed when:
+    /// <list type="bullet">
+    ///     <item>they are removed from the list</item>
+    ///     <item>the list is disposed itself</item>
+    /// </list>
+    /// </remarks>
     /// <typeparam name="T">
     ///     The tag model type. Either a <see cref="TagModel"/> or a sub-class of it.
     /// </typeparam>
@@ -36,14 +41,24 @@ namespace WetHatLab.OneNote.TaggingKit.common
         IEnumerator<object> IEnumerable<object>.GetEnumerator() => GetEnumerator();
         #endregion IObservableTagList
 
+        /// <summary>
+        /// Create a new instance of an observable list of tag view models.
+        /// </summary>
+        /// <remarks>
+        ///     The list is configured to dispose view model on removal
+        ///     by default
+        /// </remarks>
+        public ObservableTagList() {
+            DisposeRemovedItems = true;
+        }
+
         #region IDisposable
+        /// <summary>
+        /// Dispose all items in this list.
+        /// </summary>
         public override void Dispose() {
+            Clear(); // empty list so that all items get disposed.
             base.Dispose();
-            // dispose all items in this list
-            foreach (var itm in this) {
-                itm.Dispose();
-            }
-            Clear();
         }
         #endregion IDisposable
     }
