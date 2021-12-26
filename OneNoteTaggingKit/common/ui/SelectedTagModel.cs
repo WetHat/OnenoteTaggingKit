08673514af58241b4eb7c2e4bc2d1c61ab83211a
WetHat <PeterErnst@t-online.de>
2021-12-26 12:43:14 +0100

    Decorator view model for selected tags

---
+++ OneNoteTaggingKit/common/ui/SelectedTagModel.cs
@@ -0,0 +1,37 @@
+﻿namespace WetHatLab.OneNote.TaggingKit.common.ui
+{
+    /// <summary>
+    /// A view model which decorates instances of <see cref="FilterableTagModel"/>
+    /// where the <see cref="FilterableTagModel.IsSelected"/> is `true`.
+    /// </summary>
+    /// <remarks>
+    /// When disposed sets <see cref="FilterableTagModel.IsSelected"/> of the
+    /// decorated view model to `false`.
+    /// </remarks>
+    public class SelectedTagModel : TagModel
+    {
+
+        FilterableTagModel _selectableTag = null;
+        /// <summary>
+        /// Get/set the view model of a tag that can be selected
+        /// </summary>
+        /// <remarks>
+        ///     As a side-efect also sets the <see cref="TagModel.TagName"/>
+        ///     property inherited from its base class.
+        /// </remarks>
+        public FilterableTagModel SelectableTag {
+            get => _selectableTag;
+            set {
+                _selectableTag = value;
+                TagName = value.TagName;
+            }
+        }
+
+        /// <summary>
+        /// Create a new instance of this decorator class.
+        /// </summary>
+        public SelectedTagModel() {
+            TagIndicator = "❌";
+        }
+    }
+}
