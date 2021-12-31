b593d7d83b1e1ce39934b2d78a1d2230c8953333
WetHat <PeterErnst@t-online.de>
2021-12-31 19:39:26 +0100

    First go at custom presets

---
+++ OneNoteTaggingKit/presets/CustomPresetsList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace WetHatLab.OneNote.TaggingKit.presets
+{
+    public class CustomPresetsList : ObservableCollection<CustomPresetModel>
+    {
+        /// <summary>
+        /// Create a new observable of custom presets from persisted data.
+        /// </summary>
+        /// <param name="presets"></param>
+        public CustomPresetsList(StringCollection presets) {
+            foreach (string p in presets) {
+                Add(new CustomPresetModel(p));
+            }
+        }
+    }
+}
