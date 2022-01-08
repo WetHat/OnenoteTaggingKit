bcab5ee5ef3d0f227b3c586f866f84a7f00c2727
WetHat <PeterErnst@t-online.de>
2022-01-08 17:20:47 +0100

    Filterable tag logic exrtracted into separate class

---
+++ OneNoteTaggingKit/common/FilterableTagsSource.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Specialized;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+
+namespace WetHatLab.OneNote.TaggingKit.common
+{
+    /// <summary>
+    /// An observable list of tag related view models which can be filtered
+    /// and highlighted by applying search criteria.
+    /// </summary>
+    /// <typeparam name="T">View model type</typeparam>
+    public class FilterableTagsSource<T> : ObservableTagList<T> where T : FilterableTagModel
+    {
+        TextSplitter _highlighter = new TextSplitter();
+
+        /// <summary>
+        /// Get/set the text highlighter applied to all tag models in this list.
+        /// </summary>
+        public TextSplitter Highlighter {
+            get => _highlighter;
+            set {
+                _highlighter = value;
+                foreach (T mdl in this) {
+                    mdl.Highlighter = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Apply additional configuration to new items in the list.
+        /// </summary>
+        /// <param name="sender">List raising the event.</param>
+        /// <param name="e">Event details.</param>
+        private void FilterableTagsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            if (e.Action == NotifyCollectionChangedAction.Add) {
+                foreach (T mdl in e.NewItems) {
+                    mdl.Highlighter = Highlighter;
+                }
+            }
+        }
+        /// <summary>
+        /// Construct an instance of the collection of new tags.
+        /// </summary>
+        public FilterableTagsSource() {
+            CollectionChanged += FilterableTagsSource_CollectionChanged;
+        }
+    }
+}
--- OneNoteTaggingKit/common/KnownTagsSource.cs
+++ OneNoteTaggingKit/common/KnownTagsSource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using WetHatLab.OneNote.TaggingKit.common.ui;
 
@@ -15,7 +11,7 @@ namespace WetHatLab.OneNote.TaggingKit.common
     /// Tags in this list are typically used to suggest tags.
     /// </remarks>
     /// <typeparam name="T">A <see cref="FilterableTagModel"/> or a subclass of it.</typeparam>
-    public class KnownTagsSource<T> : ObservableTagList<T> where T : FilterableTagModel,new()
+    public class KnownTagsSource<T> : FilterableTagsSource<T> where T : FilterableTagModel, new()
     {
         /// <summary>
         /// Asynchronously load all known tags from the persisted settings.
@@ -32,20 +28,6 @@ namespace WetHatLab.OneNote.TaggingKit.common
                     select new T() { TagName = t} ).ToArray();
         }
 
-        private TextSplitter _highlighter = new TextSplitter();
-
-        /// <summary>
-        /// Get/set the text highlighter applied to all tag models in this list.
-        /// </summary>
-        public TextSplitter Highlighter {
-            get => _highlighter;
-            set {
-                _highlighter = value;
-                foreach (T mdl in this) {
-                    mdl.Highlighter = value;
-                }
-            }
-        }
         /// <summary>
         /// Save the suggested tags to the add-in settings store.
         /// </summary>
@@ -53,31 +35,5 @@ namespace WetHatLab.OneNote.TaggingKit.common
             Properties.Settings.Default.KnownTagsCollection.Clear();
             Properties.Settings.Default.KnownTagsCollection.AddRange((from v in Values select v.TagName).ToArray());
         }
-
-        /// <summary>
-        /// Construct an instance of the collection of new tags.
-        /// </summary>
-        public KnownTagsSource() {
-            CollectionChanged += KnownTagsSource_CollectionChanged;
-        }
-
-        /// <summary>
-        /// Apply additional configuration to new items in the list.
-        /// </summary>
-        /// <param name="sender">List raising the event.</param>
-        /// <param name="e">Event details.</param>
-        private void KnownTagsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
-            if (e.Action == NotifyCollectionChangedAction.Add) {
-                foreach (T mdl in e.NewItems) {
-                    mdl.Highlighter = Highlighter;
-                }
-            }
-        }
-
-        #region IDisposable
-        public override void Dispose() {
-            base.Dispose();
-        }
-        #endregion IDisposable
     }
 }
