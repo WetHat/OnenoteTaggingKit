9afda9f212234dd5f16c4b3f5d5918b7627c00e2
WetHat <PeterErnst@t-online.de>
2022-01-09 20:42:39 +0100

    Implemented affinity to the thread in which the list has been created,

--- OneNoteTaggingKit/common/ObservableTagList.cs
+++ OneNoteTaggingKit/common/ObservableTagList.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using WetHatLab.OneNote.TaggingKit.common.ui;
+using System.Windows.Threading;
 
 namespace WetHatLab.OneNote.TaggingKit.common
 {
@@ -23,11 +24,15 @@ namespace WetHatLab.OneNote.TaggingKit.common
     /// A generic observable list of sorted page tag models.
     /// </summary>
     /// <remarks>
-    /// Items in this list are disposed when:
-    /// <list type="bullet">
-    ///     <item>they are removed from the list</item>
-    ///     <item>the list is disposed itself</item>
-    /// </list>
+    ///     Items in this list are disposed when:
+    ///     <list type="bullet">
+    ///         <item>they are removed from the list</item>
+    ///         <item>the list is disposed itself</item>
+    ///     </list>
+    ///
+    ///     Instances of  this observable list have affinity to the thread they
+    ///     were created by. Collection change events are raised in the context
+    ///     of that thread.
     /// </remarks>
     /// <typeparam name="T">
     ///     The tag model type. Either a <see cref="TagModel"/> or a sub-class of it.
@@ -36,10 +41,59 @@ namespace WetHatLab.OneNote.TaggingKit.common
                                                       where T : TagModel {
         #region IObservableTagList
         object IReadOnlyList<object>.this[int index] => this[index];
-
+        /// <summary>
+        /// Get all tags as object list.
+        /// </summary>
+        /// <returns>Generic object list.</returns>
         public IList ToTagList() => Values.ToArray();
         IEnumerator<object> IEnumerable<object>.GetEnumerator() => GetEnumerator();
         #endregion IObservableTagList
+
+        Dispatcher _dispatcher;
+
+        /// <summary>
+        /// Event raised when the list of tags has changed,
+        /// </summary>
+        /// <remarks>
+        ///     The events are raised in the context of the thread in which this
+        ///     observable list instance was generated.
+        /// </remarks>
+        public override event NotifyCollectionChangedEventHandler CollectionChanged {
+            add {
+                var adapter = new EventAdapter(_dispatcher, value);
+                base.CollectionChanged += adapter.Handler;
+            }
+            remove {
+                throw new NotImplementedException("Event handlers cannot be removed!");
+            }
+        }
+
+        /// <summary>
+        /// Adapter class facilitate raising events in the tread context this list
+        /// instance was created in,.
+        /// </summary>
+        private class EventAdapter
+        {
+            NotifyCollectionChangedEventHandler _originalHandler;
+            Dispatcher _dispatcher;
+            /// <summary>
+            /// initialize a new event adapter object.
+            /// </summary>
+            /// <param name="d">
+            ///     Dispatcher of the thread which created the observable list.
+            /// </param>
+            /// <param name="handler">
+            ///     Event handler to call in the given dispater's context.
+            /// </param>
+            public EventAdapter(Dispatcher d, NotifyCollectionChangedEventHandler handler) {
+                _dispatcher = d;
+                _originalHandler = handler;
+            }
+
+            public void Handler(object sender, NotifyCollectionChangedEventArgs args) {
+                _dispatcher.Invoke(() => _originalHandler(sender, args));
+            }
+        }
 
         /// <summary>
         /// Create a new instance of an observable list of tag view models.
@@ -50,6 +104,7 @@ namespace WetHatLab.OneNote.TaggingKit.common
         /// </remarks>
         public ObservableTagList() {
             DisposeRemovedItems = true;
+            _dispatcher = Dispatcher.CurrentDispatcher;
         }
 
         #region IDisposable
