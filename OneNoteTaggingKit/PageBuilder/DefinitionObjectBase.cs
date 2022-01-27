34413263cd3be301db5ee98c986927f511128e60
WetHat <PeterErnst@t-online.de>
2022-01-27 11:51:09 +0100

    Object model of definition objects

---
+++ OneNoteTaggingKit/PageBuilder/DefinitionObjectBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace WetHatLab.OneNote.TaggingKit.PageBuilder
+{
+    /// <summary>
+    /// Base class for indexed definition proxy objects.
+    /// </summary>
+    /// <remarks>
+    ///     Definition objects are referred to by index by page content
+    ///     elements.
+    /// </remarks>
+    public class DefinitionObjectBase : KeyedObjectBase, IDisposable {
+
+        int _index;
+        /// <summary>
+        /// Get the index of the definition object
+        /// </summary>
+        public int Index {
+            get => _index;
+            set {
+                _index = value;
+                SetAttributeValue("index", value.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Initialize a definition object proxy with a
+        /// XML element existing on a OneNote page.
+        /// </summary>
+        /// <remarks>
+        ///     Definition elements must have:
+        ///     <list type="bullet">
+        ///         <item>a `name` property containing a key
+        ///               which is unique to the page.</item>
+        ///         <item>an `index` property containing a unique index (integer)
+        ///               which is used by content elements to refer to the
+        ///               definition</item>
+        ///     </list>
+        /// </remarks>
+        /// <param name="element">
+        ///     An XML element with an index attribute
+        ///     on a OneNote page.
+        /// </param>
+        public DefinitionObjectBase(XElement element) : base(element) {
+            _index = int.Parse(GetAttributeValue("index"));
+        }
+
+        /// <summary>
+        /// Initialize a new keyed OneNote page element
+        /// </summary>
+        /// <param name="name">The Xml element name</param>
+        /// <param name="key">the unique name</param>
+        /// <param name="index">the index of the element</param>
+        protected DefinitionObjectBase(XName name, int index, string key) : base(name, key) {
+            Index = index;
+        }
+
+        #region IDisposable
+        /// <summary>
+        /// Determine if this definition has been dsposed.
+        /// </summary>
+        public bool IsDisposed => Element == null;
+        /// <summary>
+        /// Dispose a definition object.
+        /// </summary>
+        public void Dispose() {
+            if (!IsDisposed) {
+                Element.Remove();
+            }
+            Element = null;
+        }
+        #endregion IDisposable
+    }
+}
---
+++ OneNoteTaggingKit/PageBuilder/DefinitionObjectCollection.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace WetHatLab.OneNote.TaggingKit.PageBuilder
+{
+    /// <summary>
+    /// An abstract base class for definition object collections.
+    /// </summary>
+    /// <remarks>
+    ///     When items get removed from the collection, the corresponding XML
+    ///     elements get removed from the page but their slot in the collections
+    ///     does not get recycled, so that all other items retain their index.
+    /// </remarks>
+    /// <typeparam name="T">Proxy object type.</typeparam>
+    public abstract class DefinitionObjectCollection<T> : PageStructureObjectCollection<T> where T: DefinitionObjectBase
+    {
+        /// <summary>
+        /// Initialize an instance of this collection for elements with a
+        /// given name found on a OneNote page XML document .
+        /// </summary>
+        /// <param name="page">The OneNote page Xml document.</param>
+        /// <param name="position">THe position of elemnts of this type on a OneNote page.</param>
+        public DefinitionObjectCollection(XDocument page,PagePosition position) : base (page,position) {
+            foreach (var m in page.Root.Elements(ElementName)) {
+                Items.InsertRange(0, from xe in page.Root.Elements(ElementName)
+                                        select CreateElement(xe));
+            }
+        }
+
+        /// <summary>
+        /// Dispose a definition.
+        /// </summary>
+        /// <remarks>
+        ///     The XML element associated with the item is removed from the
+        ///     page, but the item's slot in the collection is not recycled.
+        /// </remarks>
+        /// <param name="index">Index of the item to dispose</param>
+        public void Dispose(int index) {
+            Items[index].Dispose();
+        }
+
+        /// <summary>
+        /// Add a new proxy obkect to the end of the collection.
+        /// </summary>
+        /// <param name="proxy"></param>
+        public override void Add(T proxy) {
+            proxy.Index = Items.Count;
+        }
+    }
+}
