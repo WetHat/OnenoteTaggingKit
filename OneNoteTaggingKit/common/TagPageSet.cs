ef6ebffead33d3a1b8f2c7473a7754e34512beb2
WetHat <PeterErnst@t-online.de>
2022-12-30 19:20:53 +0100

    Moving filtering to the RefinementTag decorator class. Some cleanup

--- OneNoteTaggingKit/common/TagPageSet.cs
+++ OneNoteTaggingKit/common/TagPageSet.cs
@@ -8,7 +8,7 @@ namespace WetHatLab.OneNote.TaggingKit.common
     /// The set of pages which have a particular tag in their &lt;one:Meta
     /// name="TaggingKit.PageTags" ...&gt; meta element.
     /// </summary>
-    /// <remarks>A filter can be applied to constrain the number of tagged pages <see cref="IntersectWith" /></remarks>
+    /// <remarks>Instances of this class can be collected in hashtables.</remarks>
     public class TagPageSet : ObservableObject, IKeyedItem<string> {
 
         /// <summary>
@@ -21,14 +21,6 @@ namespace WetHatLab.OneNote.TaggingKit.common
         /// </summary>
         /// <value>Basename of the tag without type annotation.</value>
         public string TagName { get => Tag.BaseName; }
-
-        /// <summary>
-        /// Get the tag's type marker.
-        /// </summary>
-        /// <value>A marker string</value>
-        public string TagType {
-            get => Tag.TagMarker;
-        }
 
         /// <summary>
         /// Initialize a new instance of a set of pages having the given page
@@ -45,61 +37,12 @@ namespace WetHatLab.OneNote.TaggingKit.common
         /// </summary>
         public ISet<PageNode> Pages { get => _pages; }
 
-        int _filteredPageCount = 0;
-        /// <summary>
-        /// Observable property of the number of pages after filter application.
-        /// </summary>
-        public int FilteredPageCount {
-            get {
-               return _filteredPages == null ? Pages.Count :  _filteredPageCount;
-            }
-            set {
-                if (_filteredPageCount != value) {
-                    _filteredPageCount = value;
-                    RaisePropertyChanged();
-                }
-            }
-        }
-
-        ISet<PageNode> _filteredPages = null;
-        /// <summary>
-        /// Get the set of tags after filtering.
-        /// </summary>
-        /// <value>
-        /// If no filter is applied returns the set of all pages
-        /// associated wth this tag otherwise returnes the set of
-        /// pages with the filter applied.
-        /// </value>
-        internal ISet<PageNode> FilteredPages => _filteredPages ?? _pages;
-
         /// <summary>
         /// Associate a page with this page.
         /// </summary>
         /// <param name="pg">Page having this tag.</param>
         /// <returns></returns>
         internal bool AddPage(PageNode pg) => Pages.Add(pg);
-
-        /// <summary>
-        /// Clear the tag filter.
-        /// </summary>
-        public void ClearFilter()
-        {
-            _filteredPages = null;
-            FilteredPageCount = Pages.Count;
-        }
-
-        /// <summary>
-        /// Apply an intersection filter to constrain the number of pages.
-        /// </summary>
-        /// <param name="filter"></param>
-        internal void IntersectWith(IEnumerable<PageNode> filter)
-        {
-            _filteredPages = new HashSet<PageNode>(filter);
-            FilteredPages.IntersectWith(Pages);
-            FilteredPageCount = FilteredPages.Count;
-        }
-
-        #region IKeyedItem
         /// <summary>
         /// Determine if two tags a are equal based on their tagname.
         /// </summary>
@@ -120,7 +63,7 @@ namespace WetHatLab.OneNote.TaggingKit.common
         /// <returns>hashcode</returns>
         public override int GetHashCode() => Tag.GetHashCode();
 
-
+        #region IKeyedItem
         /// <summary>
         /// Get the tag's sortable key
         /// </summary>
---
+++ OneNoteTaggingKit/find/RefinementTag.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.HierarchyBuilder;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    ///     A decorator for OneNote page tags used to filter sets of OneNote pages.
+    /// </summary>
+    public class RefinementTag : ObservableObject, IKeyedItem<string>
+    {
+        /// <summary>
+        ///     Get the tag appearing on a set of OneNote pages.
+        /// </summary>
+        public TagPageSet Tag { get; private set; }
+
+        /// <summary>
+        ///     Get the set of pages having the tag represented by this object.
+        /// </summary>
+        public ISet<PageNode> Pages { get => Tag.Pages; }
+
+        /// <summary>
+        ///     Get name of the page tag.
+        /// </summary>
+        /// <value>
+        ///     Basename of the tag without type annotation.
+        /// </value>
+        public string TagName { get => Tag.TagName; }
+
+        /// <summary>
+        ///     Initialize a refinement tag with a tag used on one or more OneNote pages.
+        /// </summary>
+        /// <param name="tag">
+        ///     A tag ued on OneNote pages.
+        /// </param>
+        public RefinementTag (TagPageSet tag) {
+            Tag = tag;
+        }
+
+        int _filteredPageCount = -1;
+        /// <summary>
+        ///     Observable number of pages having this tag after filter application.
+        /// </summary>
+        public int FilteredPageCount {
+            get {
+                return _filteredPageCount < 0 ? Pages.Count : _filteredPageCount;
+            }
+            set {
+                if (_filteredPageCount != value) {
+                    _filteredPageCount = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Determine how many pages having this tag match the filter.
+        /// </summary>
+        /// <param name="filter">A set of OneNote pages.</param>
+        internal void IntersectWith(IEnumerable<PageNode> filter) {
+            var filterset = new HashSet<PageNode>(filter);
+            filterset.IntersectWith(Pages);
+            FilteredPageCount = filterset.Count;
+        }
+
+        /// <summary>
+        ///     Clear the tag filter.
+        /// </summary>
+        public void ClearFilter() {
+            FilteredPageCount = -1;
+        }
+
+        /// <summary>
+        ///     Determine if two tags a are equal based on their tagname.
+        /// </summary>
+        /// <param name="obj">the other tag for equality test</param>
+        /// <returns>
+        ///     true if both tags are equal; false otherwise.
+        ///     </returns>
+        public override bool Equals(object obj) {
+            TagPageSet other = obj as TagPageSet;
+
+            if (other != null) {
+                return Tag.TagName.Equals(other.TagName);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Get the tag's hashcode.
+        /// </summary>
+        /// <returns>hashcode</returns>
+        public override int GetHashCode() => Tag.GetHashCode();
+
+        #region IKeyedItem<string>
+        /// <summary>
+        /// Get the unique key of the tag.
+        /// </summary>
+        public string Key => Tag.Key;
+        #endregion IKeyedItem<string>
+    }
+}
