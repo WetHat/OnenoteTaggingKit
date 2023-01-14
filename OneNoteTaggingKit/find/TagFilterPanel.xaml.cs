cb2bac916022e180ddd31edc82e15e913fcd6858
WetHat <PeterErnst@t-online.de>
2023-01-14 19:39:56 +0100

    Issue #20 : Control implemenmted to configure tag filters based on varius strategies

---
+++ OneNoteTaggingKit/find/TagFilterPanel.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    ///     interaction logic for the user control.
+    /// </summary>
+    [ComVisible(false)]
+    public partial class TagFilterPanel : UserControl
+    {
+        #region NotificationProperty
+        /// <summary>
+        /// Dependecy property definition for <see cref="Notification"/>
+        /// </summary>
+        public static readonly DependencyProperty NotificationProperty = DependencyProperty.Register(
+            nameof(Notification),
+            typeof(string),
+            typeof(TagFilterPanel));
+
+        /// <summary>
+        /// Get or set the popup notification which is displayed for a few second
+        /// over this list control.
+        /// </summary>
+        public string Notification {
+            get => GetValue(NotificationProperty) as string;
+            set => SetValue(NotificationProperty, value);
+        }
+        #endregion NotificationProperty
+
+        #region Event Handlers
+        private void SelectedTag_TagClick(object sender, RoutedEventArgs e) {
+            if (e.OriginalSource is Tag tagBtn
+                && tagBtn.DataContext is SelectedTagModel sel) {
+                sel.SelectableTag.IsSelected = false;
+            }
+            tagInput.FocusInput();
+        }
+        void SelectableTag_TagSelected(object sender, RoutedEventArgs e) {
+            if (sender is SelectableTag btn
+                && btn.DataContext is RefinementTagModel mdl) {
+                mdl.IsSelected = true;
+            }
+        }
+        async void TagInputBox_Input(object sender, TagInputEventArgs e) {
+            pBar.Visibility = Visibility.Visible;
+            ViewModel.RefinementTagModels.Highlighter = tagInput.IsEmpty ? new TextSplitter() : new TextSplitter(e.Tags);
+            if (e.TagInputComplete && !tagInput.IsEmpty) {
+                // select all tags with exact full matches
+                await SelectAllMatchingTagsAsync();
+            } else if (e.Action == TagInputEventArgs.TaggingAction.Clear) {
+                ClearSelectionButton_Click(sender, e);
+            }
+            pBar.Visibility = Visibility.Hidden;
+        }
+        private async void ClearSelectionButton_Click(object sender, RoutedEventArgs e) {
+            pBar.Visibility = Visibility.Visible;
+            await ViewModel.ClearFilterAsync();
+            pBar.Visibility = Visibility.Hidden;
+            e.Handled = true;
+        }
+        private async void SelectMatchingTagsButton_Click(object sender, RoutedEventArgs e) {
+            pBar.Visibility = Visibility.Visible;
+            await SelectAllMatchingTagsAsync();
+            pBar.Visibility = Visibility.Hidden;
+        }
+        private void TagFilterPanel_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            ViewModel = DataContext as TagFilterPanelModel;
+        }
+        #endregion Event Handlers
+
+        async Task SelectAllMatchingTagsAsync() {
+            if (!tagInput.IsEmpty) {
+                pBar.Visibility = Visibility.Visible;
+                if (tagInput.IsPreset) {
+                    await ViewModel.ResetFilterAsync(from mdl in ViewModel.RefinementTagModels.Values
+                                                     where mdl.IsFullMatch
+                                                     select mdl);
+                } else {
+                    await ViewModel.AddAllTagsToFilterAsync(from mdl in ViewModel.RefinementTagModels.Values
+                                                            where mdl.IsFullMatch && !mdl.IsSelected
+                                                            select mdl);
+                }
+                pBar.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private TagFilterPanelModel _model;
+        /// <summary>
+        /// get or set the view model backing this UI
+        /// </summary>
+        private TagFilterPanelModel ViewModel {
+            get => _model;
+            set {
+                _model = value;
+                DataContext = _model;
+            }
+        }
+
+        /// <summary>
+        /// Initialze a new user control instance.
+        /// </summary>
+        public TagFilterPanel() {
+            InitializeComponent();
+            pBar.Visibility = Visibility.Hidden;
+            DataContextChanged += TagFilterPanel_DataContextChanged;
+        }
+    }
+}
---
+++ OneNoteTaggingKit/find/TagFilterPanelModel.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using WetHatLab.OneNote.TaggingKit.common;
+using WetHatLab.OneNote.TaggingKit.common.ui;
+
+namespace WetHatLab.OneNote.TaggingKit.find
+{
+    /// <summary>
+    ///     View model backing the <see cref="TagFilterPanel"/> control.
+    /// </summary>
+    public class TagFilterPanelModel : ObservableObject {
+        /// <summary>
+        ///     Get the observable list of view models of tags selected for refinement.
+        /// </summary>
+        public ObservableTagList<SelectedTagModel> SelectedTagModels { get; } = new ObservableTagList<SelectedTagModel>();
+
+        TagsAndPages _ContextTagSource;
+        /// <summary>
+        ///     Get the source of tags found in a OneNote hierarchy context (section,
+        ///     section group, notebook).
+        /// </summary>
+        /// <remarks>
+        ///     This source object is used to control the tag presets.
+        /// </remarks>
+        public TagsAndPages ContextTagSource {
+            get => _ContextTagSource;
+            private set {
+                _ContextTagSource = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Get the OneNote application object proxy.
+        /// </summary>
+        OneNoteProxy OneNoteApp { get; set; }
+
+        /// <summary>
+        ///     Get a source for <see cref="RefinementTagModel"/> view models.
+        /// </summary>
+        public RefinementTagModelSource RefinementTagModels { get; private set; }
+
+        string _RefinementTagsPanelHeader = string.Empty;
+
+        /// <summary>
+        ///     Get the header text for the panel displaying the tags
+        ///     available for filter selection.
+        /// </summary>
+        public string RefinementTagsPanelHeader {
+            get => _RefinementTagsPanelHeader;
+            private set {
+                if (!value.Equals(_RefinementTagsPanelHeader)) {
+                    _RefinementTagsPanelHeader = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        void UpdateRefinementTagsPanelHeader() {
+            RefinementTagsPanelHeader = SelectedTagModels.Count == 0
+                ? string.Format("{0} ({1})",
+                                Properties.Resources.TagSearch_Tags_GroupBox_Title,
+                                RefinementTagModels.Count)
+                : string.Format("{0} (Ո)",
+                                Properties.Resources.TagSearch_Tags_GroupBox_Title);
+        }
+
+        string _SelectedTagsPanelHeader = string.Empty;
+        /// <summary>
+        /// Get the header text for the panel displaying selected tags.
+        /// </summary>
+        public string SelectedTagsPanelHeader {
+            get => _SelectedTagsPanelHeader;
+            private set {
+                if (!value.Equals(_SelectedTagsPanelHeader)) {
+                    _SelectedTagsPanelHeader = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        /// <summary>
+        /// Add a single tag to the refinement filter.
+        /// </summary>
+        /// <param name="tag">View model of the tag.</param>
+        internal void AddTagToFilter(RefinementTagModel tag) {
+            var rt = tag.RefinementTag;
+            Filter.SelectedTags.Add(rt.Key,rt.TagWithPages);
+        }
+
+        /// <summary>
+        ///     Clear the tag filter.
+        /// </summary>
+        /// <returns>Awaitable task object.</returns>
+        public Task ClearFilterAsync() {
+            SelectedTagModels.Clear();
+            return RefinementTagModels.ClearFilterAsync();
+        }
+
+        /// <summary>
+        ///     Reset the filter tags to a given collection of tags
+        /// </summary>
+        /// <param name="tags">Refinement tag collection.</param>
+        /// <returns>Awaitable task object.</returns>
+        public Task ResetFilterAsync(IEnumerable<RefinementTagModel> tags) {
+            return RefinementTagModels.ResetFilterAsync(tags);
+        }
+
+        /// <summary>
+        ///     Add given refinement tags to the filter,
+        /// </summary>
+        /// <param name="tags">tags to add to the filter</param>
+        /// <returns>Awaitable task object.</returns>
+        public Task AddAllTagsToFilterAsync(IEnumerable<RefinementTagModel> tags) {
+            return RefinementTagModels.AddAllTagsToFilterAsync(tags);
+        }
+
+        /// <summary>
+        ///     Update the header of the panel displaying selected tags.
+        /// </summary>
+        void UpdateSelectedTagsPanelHeader() {
+            SelectedTagsPanelHeader = string.Format("{0} ({1})",
+                                                       Properties.Resources.TagSearch_SelectedTags_GroupBox_Title,
+                                                       SelectedTagModels.Count);
+        }
+        /// <summary>
+        ///     Handle chenges to the collection of refinement tags.
+        /// </summary>
+        /// <param name="sender">The collection which raised the changed event.</param>
+        /// <param name="e">Change details.</param>
+        private void TagSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            UpdateRefinementTagsPanelHeader();
+        }
+
+        /// <summary>
+        ///     Handle changes to the tag filter.
+        /// </summary>
+        /// <remarks>
+        ///     Keeps the <see cref="SelectedTagModels"/> in sync with the filter.
+        /// </remarks>
+        /// <param name="sender">
+        ///     The collection of filter tags which raised the change event.
+        /// </param>
+        /// <param name="e">Chenge details</param>
+        private void SelectedTags_CollectionChanged(object sender, NotifyDictionaryChangedEventArgs<string, TagPageSet> e) {
+            switch (e.Action) {
+                case NotifyDictionaryChangedAction.Add:
+                    SelectedTagModels.AddAll(from t in e.Items
+                                             select new SelectedTagModel() {
+                                                 SelectableTag = RefinementTagModels[t.Key],
+                                                 TagIndicator = "",
+                                                 TagIndicatorColor = Brushes.Red
+                                             });
+                    break;
+                case NotifyDictionaryChangedAction.Remove:
+                    SelectedTagModels.RemoveAll(from t in e.Items select t.Key);
+                    break;
+                case NotifyDictionaryChangedAction.Reset:
+                    SelectedTagModels.Clear();
+                    SelectedTagModels.AddAll(from t in Filter.SelectedTags.Values
+                                             select new SelectedTagModel() {
+                                                 SelectableTag = RefinementTagModels[t.Key],
+                                                 TagIndicator = "",
+                                                 TagIndicatorColor = Brushes.Red
+                                             });
+                    break;
+            }
+            UpdateRefinementTagsPanelHeader();
+            UpdateSelectedTagsPanelHeader();
+        }
+
+        /// <summary>
+        ///     Get the filter strategy for this model.
+        /// </summary>
+        public TagFilterBase Filter { get; private set; }
+
+        /// <summary>
+        ///     Initialize a new instance of a backing model for the
+        ///     <see cref="TagFilterPanel"/> control.
+        /// </summary>
+        /// <param name="onenote">OneNote application proxy.</param>
+        /// <param name="filter">The filter strategy.</param>
+        public TagFilterPanelModel(OneNoteProxy onenote,  TagFilterBase filter) {
+            Filter = filter;
+            ContextTagSource = new TagsAndPages(onenote);
+            RefinementTagModels = new RefinementTagModelSource(filter);
+            RefinementTagModels.CollectionChanged += TagSource_CollectionChanged;
+            filter.SelectedTags.CollectionChanged += SelectedTags_CollectionChanged;
+            UpdateRefinementTagsPanelHeader();
+            UpdateSelectedTagsPanelHeader();
+            OneNoteApp = onenote;
+        }
+    }
+}
