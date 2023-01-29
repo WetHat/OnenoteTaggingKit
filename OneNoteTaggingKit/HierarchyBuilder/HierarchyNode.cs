fe6617fe710913a1f8fce94b45f240ad28dff481
WetHat <PeterErnst@t-online.de>
2023-01-29 15:23:41 +0100

    Issue #83 a marker to breadcrumbs to make them recognizable as headings. Also now using the standard breadrumb separator.

--- OneNoteTaggingKit/HierarchyBuilder/HierarchyNode.cs
+++ OneNoteTaggingKit/HierarchyBuilder/HierarchyNode.cs
@@ -70,7 +70,7 @@ namespace WetHatLab.OneNote.TaggingKit.HierarchyBuilder
                 for (var p = Parent; p != null; p = p.Parent) {
                     path.Push(p.Name);
                 }
-                return string.Join(">", path);
+                return "≡ " + string.Join(" > ", path);
             }
         }
 
